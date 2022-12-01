7dc191f4c06cfe3ef3e3e61a0427c2546bc6774b
Marko Kaartinen <marko@elysium.fi>
2022-12-01 16:37:03 +0200

    Bit of styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 <x-app>
-    <h1 class="text-3xl font-bold text-aoc-green mb-12 leading-none">Advent of Code 2022</h1>
+    <h1 class="text-5xl font-bold text-aoc-green mb-12 leading-none">Advent of Code 2022</h1>
 
     <div class="flex">
         <div class="mr-2 mb-2">
-            <a class="border-2 uppercase font-bold tracking-wider border-dashed px-3 py-2 hover:border-aoc-green hover:text-aoc-green" href="{{ route('day1') }}">Day 1</a>
+            <a class="border-2 uppercase font-bold tracking-wider border-dashed px-3 py-2 hover:border-aoc-green hover:text-aoc-green text-xl" href="{{ route('day1') }}">Day 1</a>
         </div>
     </div>
 </x-app>

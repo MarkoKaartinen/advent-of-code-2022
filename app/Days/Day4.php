229dd23b530ec4ae2a18e72eb95aebd109584308
Marko Kaartinen <marko@elysium.fi>
2022-12-04 07:39:06 +0200

    Day 4 ugly version

---
+++ app/Days/Day4.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Days;
+
+use App\Interfaces\DayInterface;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class Day4 implements DayInterface
+{
+    public Collection $pairs;
+
+    public function __construct($pairs = null)
+    {
+        if(is_a($pairs, Collection::class)) {
+            $this->pairs = $pairs;
+        }else{
+            $this->pairs = collect(Str::of(Storage::disk('root')->get('/inputs/day4.txt'))->explode("\n"));
+        }
+    }
+
+    public function part1(): int
+    {
+        $containingPairs = 0;
+        foreach ($this->pairs as $pair){
+            $elves = Str::of($pair)->explode(',');
+            $elf1sections = Str::of($elves->get(0))->explode('-');
+            $elf2sections = Str::of($elves->get(1))->explode('-');
+
+            if($elf1sections[0] <= $elf2sections[0] && $elf1sections[1] >= $elf2sections[1]){
+                $containingPairs++;
+            }elseif($elf2sections[0] <= $elf1sections[0] && $elf2sections[1] >= $elf1sections[1]){
+                $containingPairs++;
+            }
+        }
+        return $containingPairs;
+    }
+
+    public function part2()
+    {
+        $containingPairs = 0;
+        foreach ($this->pairs as $pair){
+            $elves = Str::of($pair)->explode(',');
+            $elf1sections = Str::of($elves->get(0))->explode('-');
+            $elf2sections = Str::of($elves->get(1))->explode('-');
+
+            if($elf1sections[1] >= $elf2sections[0] && $elf2sections[1] >= $elf1sections[0]) {
+                $containingPairs++;
+            }
+        }
+        return $containingPairs;
+    }
+}
---
+++ tests/Feature/Day4Test.php
@@ -0,0 +1,21 @@
+<?php
+use App\Days\Day4;
+
+beforeEach(function (){
+    $this->input = '2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8';
+    $this->day = new Day4(collect(Str::of($this->input)->explode("\n")));
+
+});
+
+test('Day 4: Part 1', function (){
+    $this->assertEquals(2, $this->day->part1());
+});
+
+test('Day 4: Part 2', function (){
+    $this->assertEquals(4, $this->day->part2());
+});
